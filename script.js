88a32b60a28365183f74441e583a3fb1e6d0200c
Majid778 <majidibrahim778@gmail.com>
2023-02-26 16:57:41 +0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let insidebridge = document.getElementById("Inside");
+
+
+
+// displaytext when insidebridge is clicked
+insidebridge.addEventListener("click", () =>{
+  document.getElementById("text").innerText = "You are inside the bridge."
+});
